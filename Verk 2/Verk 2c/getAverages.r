0d08034314491a9b99a70941e3684cc6f6e681e5
AwesomeQuest <torfit@gmail.com>
2020-05-05 15:22:00 +0000

    asdf

--- Verk 2/Verk 2c/getAverages.r
+++ Verk 2/Verk 2c/getAverages.r
@@ -1,7 +1,15 @@
-x <- read.csv("nemendur.csv")
+
+library(jsonlite)
+library(mongolite)
+
+db <- mongo(collection = "Nem", db = "Nemendur", url = "mongodb://localhost:27017")
+
+data <- db$find()
+
+x <- db$find()
 df <- data.frame(x)
 
-avgGrade <- data.frame(name=df[,1], average=rowMeans(df[,-1]), maxEink=df[which.max(df[,-1]),], minEink=df[which.min(df[,-1]),] )
+avgGrade <- data.frame(name=df[,1], average=rowMeans(df[,-1]), maxEink=apply(df[, 2:18], 1, max), minEink=apply(df[, 2:18], 1, min))
 avgGrade
 
 
