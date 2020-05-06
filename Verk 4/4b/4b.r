8b8a57c39c9e94a7ea7dbf41579f13fbea502900
AwesomeQuest <torfit@gmail.com>
2020-05-06 20:28:12 +0000

    verk3c

--- Verk 4/4b/4b.r
+++ Verk 4/4b/4b.r
@@ -1,5 +1,6 @@
 library(plyr)
 library(mongolite)
+library(data.table)
 
 
 db <- mongo(collection = "data", db = "covid19", url = "mongodb://localhost:27017")
@@ -13,7 +14,14 @@ dateSort <- df[order(df$dateRep),]
 dateSums <- ddply(dateSort,.(dateRep),summarize, cases=sum(as.numeric(cases)),deaths=sum(as.numeric(deaths)))
 plot(dateSums$dateRep, dateSums$deaths)
 
-dateSort
-
 # 2
-changeData <- mutate(dateSort, change = )
+changeData <- dateSort
+shiftdata <- rbind(c(1), changeData$cases)
+changeData <- transform(changeData, 
+                        changeCases = as.double(cases) / as.double(shiftdata))
+#3
+shiftdata <- rbind(c(1), changeData$deaths)
+changeData <- transform(changeData, 
+                        changeDeaths = as.double(deaths) / as.double(shiftdata))
+
+changeData
